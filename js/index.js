a6432854af4a19d28b722758c7c164e9bbf3c5af
Chinaemere Ike <ms.chinaemere@gmail.com>
2019-08-06 18:17:45 -0500

    Initial commit. Halfway done. Went down a rabbit hole sorry!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
 // Your code goes here
+
+//1. Mouseover
+
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', function(event){
+    console.log(event);
+    event.target.style.backgroundColor = "green";
+});
+
+//2. Keydown
+
+const nav = document.querySelector('.nav');
+nav.addEventListener('keydown', function(event){
+    console.log(event);
+    event.target.style.backgroundColor = "blue";
+});
+
+//3. Dragend
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(function(navLink){
+    navLink.addEventListener('dragend', function(event){
+        console.log(event);
+        event.target.style.opacity = "";
+    }, false)
+});
+
+//4. Resize
+
+window.addEventListener('resize', function(event){
+    document.querySelectorAll('img').forEach(function(img){
+        img.style.opacity = '0.1';
+    });
+})
+
+//5. Click
+
+const background = document.querySelector('.home');
+background.addEventListener('click', function(event){
+    console.log(event);
+    event.target.style.backgroundColor = "pink";
+});
+
+//
