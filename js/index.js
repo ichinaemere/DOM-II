2b1d510b4406f14f7211931a0bd2544d0babbfd0
Chinaemere Ike <ms.chinaemere@gmail.com>
2019-08-07 11:04:58 -0500

    Finally finished after having some issues. Apologies for the delay

--- js/index.js
+++ js/index.js
@@ -16,11 +16,11 @@ nav.addEventListener('keydown', function(event){
     event.target.style.backgroundColor = "blue";
 });
 
-//3. Dragend
+//3. Drag
 
 const navLinks = document.querySelectorAll('.nav-link');
 navLinks.forEach(function(navLink){
-    navLink.addEventListener('dragend', function(event){
+    navLink.addEventListener('drag', function(event){
         console.log(event);
         event.target.style.opacity = "";
     }, false)
@@ -32,14 +32,51 @@ window.addEventListener('resize', function(event){
     document.querySelectorAll('img').forEach(function(img){
         img.style.opacity = '0.1';
     });
-})
+    event.stopPropagation();
+});
 
 //5. Click
 
-const background = document.querySelector('.home');
+const background = document.querySelector('.intro');
 background.addEventListener('click', function(event){
     console.log(event);
     event.target.style.backgroundColor = "pink";
+    event.stopPropagation();
 });
 
-//
+//6. Dbclick
+
+const buttons = document.querySelectorAll('div.btn');
+buttons.forEach(function(button){
+    button.addEventListener('dblclick', event => {
+        console.log(event);
+        event.target.style.backgroundColor = "orange";
+    });
+});
+
+//7. Scroll
+
+window.addEventListener('scroll', function(event){
+    document.querySelectorAll('div.destination h4').forEach(function(h4){
+        h4.style.color = "yellow";
+    })
+})
+
+//8. Keyup
+const text = document.querySelectorAll("div.text-content p");
+
+window.addEventListener('keyup', event => {
+    text.forEach(photo => photo.style.display = "none")
+});
+
+//9. Click
+
+const footer = document.querySelector('footer');
+footer.addEventListener('click', function(event){
+    event.currentTarget.style.paddingRight = "200px";
+})
+
+const headerPhoto = document.querySelector('header img');
+headerPhoto.addEventListener('click', function(event){
+    event.currentTarget.style.display = "none";
+})
